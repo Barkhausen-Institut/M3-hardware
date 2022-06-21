4998686a0354ce374eebb0698831e9fac79a06a4
Sebastian Haas <sebastian.haas@barkhauseninstitut.org>
2022-06-21 22:27:26 +0200

    init files of Makefile tool flow

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+MK_TEMPLATE=Makefile.template
+MK_LOCAL=Makefile.local
+
+if [[ -f "$MK_LOCAL" ]]; then
+    echo "$MK_LOCAL already exists. If necessary, back it up, delete it, then rerun this script."
+    return
+fi
+
+cp $MK_TEMPLATE $MK_LOCAL
+
+REPO_ROOT=$(pwd)
+
+sed -i "s|#export FPGA_DESIGN|export FPGA_DESIGN=$REPO_ROOT|1" $MK_LOCAL 
+export FPGA_DESIGN=$REPO_ROOT
+
+echo $REPO_ROOT
