7aa4ed1538c80585614112a6ecedeac48b7289aa
Sebastian Haas <sebastian.haas@barkhauseninstitut.org>
2022-07-20 17:29:05 +0200

    added new testcase tc_memory_access: the test writes and reads from selected addresses of DRAM1 and DRAM2

---
+++ units/fpga_top/simulation/vivado/tc_memory_test/testcase.v
@@ -0,0 +1,72 @@
+
+//----------------------------------------------------------------------------
+// Stimulus process. This process will inject frames of data into the
+// PHY side of the receiver.
+//----------------------------------------------------------------------------
+initial
+begin : testcase
+
+    // 1 Gb/s speed
+    // wait for the internal resets to settle before staring to send traffic
+    @(posedge stim_rx_clk);
+    while (management_config_finished !== 1)
+        send_I2;
+
+    $display("ETH config finished");
+
+    repeat(100)
+        send_I2;
+
+    $display("Start Rx Stimulus, sending frames at 1G ... ");
+    $display("Send ARP frame to init UDP/IP stack");
+    send_frame(frame_arp.tobits(0));
+
+    rx_stimulus_finished = 1;
+
+
+    //---------------
+    //start testcase-specific stimulus here
+
+    write8b_noc(MODID_DRAM1, 8'hFF, 32'h1000, 64'hABC);
+    write8b_noc(MODID_DRAM2, 8'hFF, 32'h2000, 64'hCDF);
+
+    read8b_noc(MODID_DRAM1, 32'h1000);
+    read8b_noc(MODID_DRAM2, 32'h2000);
+
+    repeat(500)
+        send_I2;
+
+    $stop;
+
+
+end // testcase
+
+
+
+//----------------------------------------------------------------------------
+// Monitor process. This process checks the data coming out of the
+// transmitter.
+//----------------------------------------------------------------------------
+
+reg last_is_burst;
+
+initial
+begin : p_tx_monitor
+    last_is_burst = 1'b0;
+
+    while(rx_stimulus_finished != 1) begin
+        @(posedge mon_tx_clk);
+        #1;
+    end
+
+    $display("Check ETH frames");
+
+    forever
+        check_frame(last_is_burst, last_is_burst);
+
+    #20_000_000;
+    $stop;
+
+end
+
+
