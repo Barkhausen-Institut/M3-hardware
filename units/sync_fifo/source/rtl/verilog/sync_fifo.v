0e168bedc222055f3c1a27e8e392ac2e7627e213
Sebastian Haas <sebastian.haas@barkhauseninstitut.org>
2023-02-13 17:16:27 +0100

    sync_fifo: fix behavior of empty flag

--- units/sync_fifo/source/rtl/verilog/sync_fifo.v
+++ units/sync_fifo/source/rtl/verilog/sync_fifo.v
@@ -21,10 +21,12 @@ module sync_fifo #(
     generate
     if (USE_MEM) begin: SYNC_FIFO_MEM
 
-        reg    [ADDR_WIDTH:0] r_rd_ptr, rin_rd_ptr;
-        reg    [ADDR_WIDTH:0] r_wr_ptr, rin_wr_ptr;
-        reg                   rin_rempty;
-        reg                   r_rempty2, rin_rempty2;
+        reg [ADDR_WIDTH:0] r_rd_ptr, rin_rd_ptr;
+        reg [ADDR_WIDTH:0] r_wr_ptr, rin_wr_ptr;
+
+        reg rempty;
+        reg rempty2;
+        reg r_wr_en;
 
         wire [ADDR_WIDTH-1:0] waddr = r_wr_ptr[ADDR_WIDTH-1:0];
         wire [ADDR_WIDTH-1:0] raddr = rin_rd_ptr[ADDR_WIDTH-1:0];
@@ -34,20 +36,20 @@ module sync_fifo #(
         wire wr_en_s = wr_en_i & !wfull_o;
         wire rd_en_s = rd_en_i & !rempty_o;
 
-        //rempty2 indicates if last element in FIFO will be access
-        //if so, set FIFO to empty because mem-based FIFO needs one cycle more to release the next element
-        //in other words: if (rin_rempty2 && !r_rempty2) == 1, the last element in FIFO is the next element to be released,
-        //wait a cycle because it wasn't read from mem yet
-        assign rempty_o = rin_rempty || (rin_rempty2 && !r_rempty2);
+        //If (rempty2 && r_wr_en) == 1 then next element to be released is the one that was just written to the FIFO.
+        //In this case, raise empty flag (again) because mem-based FIFO needs one cycle more to release the next element.
+        assign rempty_o = rempty || (rempty2 && r_wr_en);
 
 
         always @(posedge clk_i or negedge resetn_i) begin
             if (resetn_i == 1'b0) begin
                 r_rd_ptr <= 'h0;
                 r_wr_ptr <= 'h0;
+                r_wr_en <= 1'b0;
             end else begin
                 r_rd_ptr <= rin_rd_ptr;
                 r_wr_ptr <= rin_wr_ptr;
+                r_wr_en <= wr_en_s;
             end
         end
 
@@ -73,27 +75,19 @@ module sync_fifo #(
             end
         end
 
-        always @(posedge clk_i or negedge resetn_i) begin
-            if (resetn_i == 1'b0) begin
-                r_rempty2 <= 1'b0;
-            end else begin
-                r_rempty2 <= wr_en_s ? 1'b0 : rin_rempty2;
-            end
-        end
-
         always @(r_rd_ptr or r_wr_ptr) begin
-            rin_rempty = 1'b0;
+            rempty = 1'b0;
             if ((r_rd_ptr[ADDR_WIDTH-1:0] == r_wr_ptr[ADDR_WIDTH-1:0]) &&
                 (r_rd_ptr[ADDR_WIDTH] == r_wr_ptr[ADDR_WIDTH])) begin
-                rin_rempty = 1'b1;
+                rempty = 1'b1;
             end
         end
 
         always @* begin
-            rin_rempty2 = 1'b0;
+            rempty2 = 1'b0;
             if ((rd_ptr2[ADDR_WIDTH-1:0] == r_wr_ptr[ADDR_WIDTH-1:0]) &&
                 (rd_ptr2[ADDR_WIDTH] == r_wr_ptr[ADDR_WIDTH])) begin
-                rin_rempty2 = 1'b1;
+                rempty2 = 1'b1;
             end
         end
 
@@ -111,7 +105,7 @@ module sync_fifo #(
             .addra  (waddr),
             .dina   (wdata_i),
 
-            .enb    (!rin_rempty),
+            .enb    (!rempty),
             .addrb  (raddr),
             .doutb  (rdata_o)
         );
