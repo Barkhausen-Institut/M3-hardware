578979aac6098984c8cbdb706a24410d5f14aeca
Sebastian Haas <sebastian.haas@barkhauseninstitut.org>
2025-09-11 09:54:48 +0200

    program_fpga.sh: pass HW index to bitfile upload script to enable multiple connected JTAG cables

--- units/fpga_top/software/fpga/program_fpga.sh
+++ units/fpga_top/software/fpga/program_fpga.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 BITFILE=$1
+JTAG_NO=$2
 
-vivado -mode batch -source $FPGA_DESIGN/units/fpga_top/software/fpga/program_fpga.tcl -tclargs $BITFILE
+vivado -mode batch -source $FPGA_DESIGN/units/fpga_top/software/fpga/program_fpga.tcl -tclargs $BITFILE $JTAG_NO
